5ea6cefbdec287701a3033fb506925ab79e18137
Hansjoerg <hansjoeg.wingeier@gmail.com>
2021-03-15 07:23:36 +0100

    cleaning pre

--- test/_99_trials/clean_pre_xml.py
+++ test/_99_trials/clean_pre_xml.py
@@ -5,30 +5,29 @@ import pandas as pd
 from typing import Dict, List, Tuple
 
 file = "c:/ieu/projects/sec_processing/data/aapl-20200926_pre.xml"
-target_num_clean_xml = "c:/ieu/projects/sec_processing/data/aapl-20200926_pre-clean.xml"
+target_pre_clean_xml = "c:/ieu/projects/sec_processing/data/aapl-20200926_pre-clean.xml"
 
-period_regex = re.compile(r"<period>|(</period>)", re.IGNORECASE + re.MULTILINE + re.DOTALL)
-entity_regex = re.compile(r"<entity>|(</entity>)", re.IGNORECASE + re.MULTILINE + re.DOTALL)
-identifier_regex = re.compile(r"(<identifier).*?(</identifier>)", re.IGNORECASE + re.MULTILINE + re.DOTALL)
-id_decimals_regex = re.compile(r"decimals[^>]*?id=\"[^<]*?\"", re.IGNORECASE + re.MULTILINE + re.DOTALL)
-textblock_regex = re.compile(r"<[^/]*?TextBlock.*?<[/].*?TextBlock.*?>", re.IGNORECASE + re.MULTILINE + re.DOTALL)
-xbrlns_regex = re.compile(r"xmlns=\".*?\"", re.IGNORECASE + re.MULTILINE + re.DOTALL)
-link_regex = re.compile(r"<link.*?>", re.IGNORECASE + re.MULTILINE + re.DOTALL)
-clean_tag_regex = re.compile(r"[{].*?[}]")
-remove_wspace_regex = re.compile(r">[\s\r\n]*<", re.IGNORECASE + re.MULTILINE + re.DOTALL)
+remove_unicode_tag_regex = re.compile(r" encoding=\"utf-8\"", re.IGNORECASE + re.MULTILINE + re.DOTALL)
+link_regex = re.compile(r"(<link:)", re.IGNORECASE + re.MULTILINE + re.DOTALL)
+link_end_regex = re.compile(r"(</link:)", re.IGNORECASE + re.MULTILINE + re.DOTALL)
+xlink_regex = re.compile(r" xlink:", re.IGNORECASE + re.MULTILINE + re.DOTALL)
+arcrole_regex = re.compile(r"http://www.xbrl.org/2003/arcrole/", re.IGNORECASE + re.MULTILINE + re.DOTALL)
+role_regex = re.compile(r"http://www.xbrl.org/2003/role/", re.IGNORECASE + re.MULTILINE + re.DOTALL)
+href_regex = re.compile(" href=\".*?#", re.IGNORECASE + re.MULTILINE + re.DOTALL)
+type_clean_regex = re.compile("( type=\"locator\")|( type=\"arc\")", re.IGNORECASE + re.MULTILINE + re.DOTALL)
+arcrole_parent_child_regex = re.compile(" arcrole=\"parent-child\"", re.IGNORECASE + re.MULTILINE + re.DOTALL)
 
-def strip_num_xml(pathtofile: str):
-    with open(pathtofile, encoding="utf8") as f:
-        data = f.read()
-        data = identifier_regex.sub("", data)
-        data = period_regex.sub("", data)
-        data = entity_regex.sub("", data)
-        data = id_decimals_regex.sub("", data)
-        data = textblock_regex.sub("", data)
-        data = xbrlns_regex.sub("", data) # clear xbrlns, so it is easier to parse
-        data = link_regex.sub("", data)
-        data = remove_wspace_regex.sub("><", data)
-
+def _strip_file(data: str) -> str:
+    """removes unneeded content from the datastring, so that xml file is easier to understand and xml parsing will be faster"""
+    data = remove_unicode_tag_regex.sub("", data)
+    data = link_regex.sub("<", data)
+    data = link_end_regex.sub("</", data)
+    data = xlink_regex.sub(" ", data)
+    data = arcrole_regex.sub("", data)
+    data = role_regex.sub("", data)
+    data = href_regex.sub(" href=\"", data)
+    data = type_clean_regex.sub("", data)
+    data = arcrole_parent_child_regex.sub("", data)
     return data
 
 
@@ -38,141 +37,14 @@ def write_to_file(filename, data):
         f.close()
 
 
-def find_last_day_of_month(datastr:str) -> str:
-    yearstr  = datastr[0:4]
-    monthstr = datastr[5:7]
-    daystr = datastr[8:]
-
-    year = int(yearstr)
-    month = int(monthstr)
-
-    last_day_of_month = calendar.monthrange(year, month)[1]
-    return yearstr + monthstr + str(last_day_of_month).zfill(2)
+def find_presentation_links(root: etree._Element):
+    presentation_links = list(root.iter('presentationLink'))
+    print(len(presentation_links))
 
 
-def calculate_qtrs(year_start_s: str, month_start_s: str, year_end_s: str, month_end_s: str) -> int:
-    year_start = int(year_start_s)
-    year_end = int(year_end_s)
-    month_start = int(month_start_s)
-    month_end = int(month_end_s) + (year_end - year_start) * 12
-
-
-    return int(round(float(month_end - month_start) / 3))
-
-
-
-def get_contexts(root: etree._Element):
-    contexts = list(root.iter('context'))
-    context_map: Dict[str, Tuple[str]] = {}
-    for context in contexts:
-        instanttxt = None
-        startdatetxt = None
-        enddatetxt = None
-
-        segments = list(context.findall('.//*[@dimension]'))
-        segments_list = []
-        for segment in segments:
-            segment_label = segment.text
-            segment_dim = segment.get("dimension")
-            segments_list.append(segment_dim + "/" + segment_label)
-
-        if len(segments_list) == 0:
-            segments_list = None
-
-        id = context.get("id")
-        instant = context.find('instant')
-        if instant is not None:
-            instanttxt = instant.text
-
-        startdate = context.find('startDate')
-        if startdate is not None:
-            startdatetxt = startdate.text
-
-        enddate = context.find('endDate')
-        if enddate is not None:
-            enddatetxt = enddate.text
-
-        if instant is None:
-            enddate = find_last_day_of_month(enddatetxt)
-            qtrs = calculate_qtrs(startdatetxt[0:4], startdatetxt[5:7], enddatetxt[0:4], enddatetxt[5:7])
-
-            context_map[id] = (enddate, qtrs, segments_list)
-        else:
-            enddate = find_last_day_of_month(instanttxt)
-            context_map[id] = (enddate, 0, segments_list)
-
-    return context_map
-
-
-def parse_cleanfile(filename) -> pd.DataFrame:
-    root = etree.parse(filename).getroot()
-
-    us_gaap_ns = root.nsmap['us-gaap']
-    pos = us_gaap_ns.rfind("/") + 1
-    versionyear = us_gaap_ns[pos:pos+4]
-
-    context_map = get_contexts(root)
-
-    tags = list(root.findall('.//*[@unitRef]'))
-
-    entries = []
-
-    for tag in tags:
-        temp_dict = {}
-
-        value_text = tag.text
-        ctxtRef = tag.get("contextRef")
-        unitRef = tag.get("unitRef").lower()
-        tagname = clean_tag_regex.sub("", tag.tag)
-        version = tag.prefix + "/" + versionyear
-        context_entry = context_map[ctxtRef]
-        ddate = context_entry[0]
-        qtrs = context_entry[1]
-        segments = context_entry[2]
-
-        if unitRef in ["usd","usdpershare"]:
-            unitRef = "USD"
-
-        temp_dict['adsh']    = ''
-        temp_dict['tag']     = tagname
-        temp_dict['version'] = version
-        temp_dict['coreg']   = ''
-        temp_dict['ddate']   = ddate
-        temp_dict['qtrs']    = qtrs
-        temp_dict['uom']     = unitRef
-        temp_dict['value']   = value_text
-        temp_dict['footnote'] = ''
-        temp_dict['segments'] = segments
-
-        entries.append(temp_dict)
-
-    return pd.DataFrame(entries)
-
-
-data = strip_num_xml(file)
-write_to_file(target_num_clean_xml, data)
-#content = parse_cleanfile(target_num_clean_xml)
-#print(len(content))
-#print(sum(~content['segments'].isnull()))
-
-# print(find_last_day_of_month("2019-11-25"))
-# print(find_last_day_of_month("2019-02-5"))
-# print(find_last_day_of_month("2020-02-5"))
-# print(find_last_day_of_month("2019-12-25"))
-
-# print(qtrs("19", "09","20","09")) #4
-# print(qtrs("19", "06","19","09")) #1
-# print(qtrs("19", "10","20", "09")) #4
-# print(qtrs("19", "08", "19", "10")) #1
-
-# Problem: es sind mehrere Werte für das gleiche Tag und das gleiche Datum vorhanden
-# z.B. CostofGoodsAndServicesSold -> im XML 9 Einträge, im CSV nur 3
-# Grund:
-# der Context hat manchmal auch ein oder mehrere Segmente:
-# <segment>
-# <xbrldi:explicitMember dimension="srt:ProductOrServiceAxis">us-gaap:ProductMember</xbrldi:explicitMember>
-# </segment>
-#
-# und die Dimension und quasi den TextInhalt sollte man mitnehmen
-# -> das ermöglicht zusätzliche Aufteilung.
-# die Zahlen, die in erster Linie interessieren, sind diejenigen ohne segment FileNotFoundError
+with open(file, "r", encoding="utf-8") as f:
+    xml_content = f.read()
+    data = _strip_file(xml_content)
+    write_to_file(target_pre_clean_xml, data)
+    root = etree.fromstring(data)
+    find_presentation_links(root)
