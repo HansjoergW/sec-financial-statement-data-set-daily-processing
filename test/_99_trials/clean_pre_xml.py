82b03b2ae0c773c6bd1fad38bc41aa040e7ecef0
Hansjoerg <hansjoeg.wingeier@gmail.com>
2021-03-16 07:27:52 +0100

    negated Flag erzeugen, dataframe anlegen

--- test/_99_trials/clean_pre_xml.py
+++ test/_99_trials/clean_pre_xml.py
@@ -12,10 +12,22 @@ link_regex = re.compile(r"(<link:)", re.IGNORECASE + re.MULTILINE + re.DOTALL)
 link_end_regex = re.compile(r"(</link:)", re.IGNORECASE + re.MULTILINE + re.DOTALL)
 xlink_regex = re.compile(r" xlink:", re.IGNORECASE + re.MULTILINE + re.DOTALL)
 arcrole_regex = re.compile(r"http://www.xbrl.org/2003/arcrole/", re.IGNORECASE + re.MULTILINE + re.DOTALL)
-role_regex = re.compile(r"http://www.xbrl.org/2003/role/", re.IGNORECASE + re.MULTILINE + re.DOTALL)
+role2003_regex = re.compile(r"http://www.xbrl.org/2003/role/", re.IGNORECASE + re.MULTILINE + re.DOTALL)
+role2009_regex = re.compile(r"http://www.xbrl.org/2009/role/", re.IGNORECASE + re.MULTILINE + re.DOTALL)
 href_regex = re.compile(" href=\".*?#", re.IGNORECASE + re.MULTILINE + re.DOTALL)
 type_clean_regex = re.compile("( type=\"locator\")|( type=\"arc\")", re.IGNORECASE + re.MULTILINE + re.DOTALL)
 arcrole_parent_child_regex = re.compile(" arcrole=\"parent-child\"", re.IGNORECASE + re.MULTILINE + re.DOTALL)
+
+
+stmt_map:Dict[str,str] = {'Cover': 'CP',
+                          'IncomeStatement': 'IS',
+                          'StatementOfIncomeAndComprehensiveIncome': 'CI',
+                          'StatementOfFinancialPosition': 'BS',
+                          'StatementOfStockholdersEquity': 'EQ',
+                          'StatementOfCashFlows': 'CF'
+                          }
+
+
 
 def _strip_file(data: str) -> str:
     """removes unneeded content from the datastring, so that xml file is easier to understand and xml parsing will be faster"""
@@ -24,7 +36,8 @@ def _strip_file(data: str) -> str:
     data = link_end_regex.sub("</", data)
     data = xlink_regex.sub(" ", data)
     data = arcrole_regex.sub("", data)
-    data = role_regex.sub("", data)
+    data = role2003_regex.sub("", data)
+    data = role2009_regex.sub("", data)
     data = href_regex.sub(" href=\"", data)
     data = type_clean_regex.sub("", data)
     data = arcrole_parent_child_regex.sub("", data)
@@ -64,39 +77,73 @@ def fill_parent_child(presentation: etree._Element, locmap: Dict[str, Tuple[str,
         locmap[parent][2].append((child, order, plabel))
 
 
+def get_prefered_label(presentation: etree._Element) -> Dict[str, str]:
+
+    dict:Dict[str, str] = {}
+    arcs = list(presentation.iter('presentationArc'))
+
+    for arc in arcs:
+        child = arc.get('to')
+        plabel = arc.get('preferredLabel')
+        dict[child] = plabel
+
+    return dict
+
+
 def simple_list(presentation: etree._Element) -> List[Dict[str, str]]:
     locs  = list(presentation.iter('loc'))
+    prefered_label_dict = get_prefered_label(presentation)
+
     result_list = []
     line = 0
     for loc in locs:
         key = loc.get("label")
+
         tag_info = loc.get("href").split("_")
         tag_prefix = tag_info[0]
         tag = tag_info[1]
-        entry = {"line": line, "version": tag_prefix, "tag": tag, "key": key}
+
+        prefered_label = prefered_label_dict.get(key)
+        negated = False
+        if prefered_label:
+            negated = "negated" in prefered_label
+
+        entry = {"line": line, "version": tag_prefix, "tag": tag, "negated": negated, "plabel": prefered_label, "key": key}
         result_list.append(entry)
         line += 1
 
     return result_list
 
-# CP CoverPage, EQ Equity, IS IncomeStatement, CI ComprehensiveIncome, BS BalanceSheet, CF CashFlow
 
-def process_presentation(presentation: etree._Element):
+def process_presentation(reportnr: int, presentation: etree._Element) -> List[Dict[str, str]]:
     # first_key, loc_map = create_loc_map(presentation)
     # fill_parent_child(presentation, loc_map)
     entries = simple_list(presentation)
     if len(entries) > 0:
-        print(entries[0]['tag'])
-    else:
-        print("---")
+        first_tag = entries[0]['tag']
+        stmt = None
+
+        for k, v in stmt_map.items():
+            if k in first_tag:
+                stmt = v
+        for entry in entries:
+            entry['report'] = reportnr
+            entry['stmt'] = stmt
+
+    return entries
 
 
-def find_presentation_links(root: etree._Element):
+def process_presentations(root: etree._Element) -> List[Dict[str, str]]:
     presentation_links = list(root.iter('presentationLink'))
     print(len(presentation_links))
-
+    report = 1
+    all_entries:List[Dict[str, str]] = []
     for presentation in presentation_links:
-        process_presentation(presentation)
+        entries = process_presentation(report, presentation)
+        all_entries.extend(entries)
+        report += 1
+
+    return all_entries
 
 
 with open(file, "r", encoding="utf-8") as f:
@@ -104,4 +151,6 @@ with open(file, "r", encoding="utf-8") as f:
     data = _strip_file(xml_content)
     write_to_file(target_pre_clean_xml, data)
     root = etree.fromstring(data)
-    find_presentation_links(root)
+    entries = process_presentations(root)
+    df = pd.DataFrame(entries)
+    print(len(df))
