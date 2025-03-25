42fa8f0739eb85bb830a1cbb811b704505f1970e
Hansjoerg <hansjoeg.wingeier@gmail.com>
2025-03-25 16:52:57 +0100

    some more cleaning

--- sandbox/trials/test_clean_htm_xml.py
+++ sandbox/trials/test_clean_htm_xml.py
@@ -6,14 +6,32 @@ CURRENT_PATH = Path(__file__).parent
 
 """
  lenght of files:
- - without stripping:         4'719'409
- - with style stripping:      1'836'532
- - with emty style stripping: 1'422'631
- - with span 160 stripping:   1'252'258
- - with empty p stripping:    1'189'489
- - with class struooubg list: 1'185'265
- - with empty td stripping:   1'105'529
- - with remove empty span:    1'103'000
+ - without stripping:                 4'719'409
+ - with style stripping:              1'836'532
+ - with emty style stripping:         1'422'631
+ - with span 160 stripping:           1'252'258
+ - with empty p stripping:            1'189'489
+ - with class struooubg list:         1'185'265
+ - with empty td stripping:           1'105'529
+ - with remove empty span:            1'103'000
+ - with remove empty lines/colspan:   1'019'060
+ - with remove ixheader:                840'807
+ - with remove span and div:            754'000
+
+ 
+
+report : https://www.sec.gov/Archives/edgar/data/1475922/000095017024123110/0000950170-24-123110-index.htm
+
+
+HTML: https://www.sec.gov/Archives/edgar/data/1475922/000095017024123110/pri-20240930.htm
+
+Extract: https://www.sec.gov/Archives/edgar/data/1475922/000095017024123110/pri-20240930_htm.xml
+   - Typen / Einheit
+   - Values
+   - Footnotes
+
+
+Notes: <ix:header><ix:hidden> -> scheint viele infos zu haben, ist auch sehr gross.. 
 
 """
 
--- src/secdaily/_02_xml/parsing/htm/_1_SecHtmXmlExtracting.py
+++ src/secdaily/_02_xml/parsing/htm/_1_SecHtmXmlExtracting.py
@@ -32,15 +32,25 @@ class SecHtmXmlExtractor():
     """ Preparses the xml content and returns a Dict, Tuple, List Structure with the relevant raw information"""
 
     remove_unicode_tag_regex = re.compile(r" encoding=\"utf-8\"", re.IGNORECASE + re.MULTILINE + re.DOTALL)
+    remove_ix_header_regex = re.compile(r"<ix:header>.*?</ix:header>", re.IGNORECASE + re.MULTILINE + re.DOTALL)
     remove_style_content_regex = re.compile(r'(?<=style=")[^"]*(?=")')
     remove_empty_style_regex = re.compile(r"( style=\"\")", re.IGNORECASE + re.MULTILINE + re.DOTALL)
+    remove_colspan_content_regex = re.compile(r'(?<=colspan=")[^"]*(?=")')
+    remove_empty_colspan_regex = re.compile(r"( colspan=\"\")", re.IGNORECASE + re.MULTILINE + re.DOTALL)
+    remove_span_attributes_regex = re.compile(r'<span\s+[^>]*>', re.IGNORECASE + re.MULTILINE + re.DOTALL)
+    remove_div_attributes_regex = re.compile(r'<div\s+[^>]*>', re.IGNORECASE + re.MULTILINE + re.DOTALL)
     remove_span_160_regex = re.compile(r"(<span>&#160;</span>)")
     remove_empty_span_regex = re.compile(r"(<span> </span>)|(<span></span>)")
     remove_empty_ps_regex = re.compile(r"<p></p>")
+    remove_dollar_ps_regex = re.compile(r"<p>\$</p>")
+    remove_dash_ps_regex = re.compile(r"<p>\-</p>")
     remove_class_content_regex = re.compile(r'(?<=class=")[^"]*(?=")')
     remove_empty_class_regex = re.compile(r"( class=\"\")", re.IGNORECASE + re.MULTILINE + re.DOTALL)
     remove_empty_tds_regex = re.compile(r"(<td></td>)|(<td/>)")
-    
+    remove_span_tags_regex = re.compile(r"(<span>)|(</span>)")
+    remove_div_tags_regex = re.compile(r"(<div>)|(</div>)")
+    remove_empty_lines_regex = re.compile(r'^\s*$\n', re.MULTILINE)
+
 
     link_regex = re.compile(r"(<link:)", re.IGNORECASE + re.MULTILINE + re.DOTALL)
     link_end_regex = re.compile(r"(</link:)", re.IGNORECASE + re.MULTILINE + re.DOTALL)
@@ -58,20 +68,31 @@ class SecHtmXmlExtractor():
 
     def _strip_file(self, data: str) -> str:
         """removes unneeded content from the datastring, so that xml parsing will be faster"""
+        data = self.remove_ix_header_regex.sub("", data)
         data = self.remove_style_content_regex.sub("", data)
         data = self.remove_empty_style_regex.sub("", data)
+        data = self.remove_colspan_content_regex.sub("", data)
+        data = self.remove_empty_colspan_regex.sub("", data)
+        data = self.remove_span_attributes_regex.sub("", data)
+        data = self.remove_div_attributes_regex.sub("", data)
         data = self.remove_span_160_regex.sub("", data)
         data = self.remove_empty_span_regex.sub("", data)
         data = self.remove_empty_ps_regex.sub("", data)
         data = self.remove_class_content_regex.sub("", data)
         data = self.remove_empty_class_regex.sub("", data)
+        data = self.remove_span_tags_regex.sub("", data)
+        data = self.remove_div_tags_regex.sub("", data)
+        data = self.remove_dollar_ps_regex.sub("", data)
+        data = self.remove_dash_ps_regex.sub("", data)
         data = self.remove_empty_tds_regex.sub("", data)
+        data = self.remove_empty_lines_regex.sub("", data)
 
-        next colspan -> before td remove
-        next empty lines removen
 
-        remove empty divs
-        consider to remove  completely "<span...>" und "</span>"  
+        # next colspan -> before td remove
+        # next empty lines removen
+
+        # remove empty divs -> gibt es nicht empty divs?
+        # consider to remove  completely "<span...>" und "</span>"  
 
 
 
